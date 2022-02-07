e2181825af1d235fd22ccab48cc02d1dc802989b
celiaaitouazzou <celiaaitouazzou@gmail.com>
2022-02-06 21:24:32 -0500

    added jfreechart

--- src/WriteLOCFile.java
+++ src/WriteLOCFile.java
@@ -25,7 +25,7 @@ public class WriteLOCFile {
 
         return javaFile + ","+ String.valueOf(loc) + "," + String.valueOf(cloc) + "," + String.valueOf(dc);
     }
-
+    
     public static void main(String[] args) {
 
     }
